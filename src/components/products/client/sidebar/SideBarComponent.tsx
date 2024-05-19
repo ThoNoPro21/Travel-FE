29d90b5000f77fd976ae5083ca28d47660a8e592
ThoNoPro21 <thoopppo45@gmail.com>
2024-05-19 16:03:32 +0700

    update

--- src/components/products/client/sidebar/SideBarComponent.tsx
+++ src/components/products/client/sidebar/SideBarComponent.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from '@/src/store/hooks';
 import { useGetCategoryQuery } from '@/src/store/queries/apiCategory.query';
 import { setMenuId } from '@/src/store/slices/common.slice';
 import { RootState } from '@/src/store/store';
-import { Badge, Card, List, Menu, MenuProps, Skeleton, Space } from 'antd';
+import { Badge, Card, List, Menu, MenuProps, Select, SelectProps, Skeleton, Space } from 'antd';
 import React, { useState } from 'react';
 
 type Props = {};
@@ -22,6 +22,12 @@ const SideBarComponent = (props: Props) => {
             label: 'Tất cả',
         },
     ];
+    const categoryMobileItems: SelectProps['options'] = [
+        {
+            value: '0',
+            label: 'Tất cả',
+        },
+    ];
     if (isSuccess_getCategory) {
         response_getCategory?.data.map((item, index) =>
             categoryItems.push({ key: item.categories_id + '', label: item.name })
@@ -31,21 +37,24 @@ const SideBarComponent = (props: Props) => {
         dispatch(setMenuId(parseInt(e.key)));
     };
     return (
-        <List
-            header={<h1 className="tw-text-base tw-font-black tw-text-center">Danh mục sản phẩm</h1>}
-            className="tw-bg-white tw-rounded-lg tw-min-h-fit"
-        >
-            <Menu
-                onClick={handleMenuBarOnClick}
-                selectedKeys={[String(selectedKeys)]}
-                items={categoryItems}
-                className="tw-text-base tw-font-bold tw-rounded-lg"
-            />
-            <div className="tw-space-y-4">
-                <Skeleton active loading={isLoading_getCategory}></Skeleton>
-                <Skeleton active loading={isLoading_getCategory}></Skeleton>
-            </div>
-        </List>
+        <>
+            <List
+                header={<h1 className="tw-text-base tw-font-black tw-text-center">Danh mục sản phẩm</h1>}
+                className="tw-bg-white tw-rounded-lg tw-min-h-fit"
+            >
+                <Menu
+                    onClick={handleMenuBarOnClick}
+                    selectedKeys={[String(selectedKeys)]}
+                    items={categoryItems}
+                    className="tw-text-base tw-font-bold tw-rounded-lg"
+                />
+                <div className="tw-space-y-4">
+                    <Skeleton active loading={isLoading_getCategory}></Skeleton>
+                    <Skeleton active loading={isLoading_getCategory}></Skeleton>
+                </div>
+            </List>
+            <Select allowClear options={categoryMobileItems} onChange={(e) => dispatch(setMenuId(parseInt(e.key)))} />
+        </>
     );
 };
 
